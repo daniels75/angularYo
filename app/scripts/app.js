16a47c4e48d1fea75af7f9176a3ccfd8c337900f
daniel.sadowski <daniel.sadowski75@gmail.com>
2014-01-25 14:50:16 +0100

    Added a AngularYo applicaton - for learning purpose

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('angularYoApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ui.bootstrap'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/myroute', {
+        templateUrl: 'views/myroute.html',
+        controller: 'MyrouteCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .when('/samples', {
+        templateUrl: 'views/samples.html',
+        controller: 'SamplesCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/samples.js
@@ -0,0 +1,33 @@
+'use strict';
+// angular.module('sampleModule', []);
+
+var mainModule = angular.module('angularYoApp');
+
+
+mainModule
+  .controller('SamplesCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+//angular.module('myModule');
+
+mainModule
+  .controller('mySampleCtr', function ($scope, notificationsService) {
+
+    $scope.addNotification = function () {
+      notificationsService.push($scope.notification);
+      $scope.notification = '';
+    };
+
+    $scope.getNotifications = function () {
+      return notificationsService.getCurrent();
+    };
+
+});
+
+mainModule.service('notificationsService', NotificationsService)
+  .value('notificationsArchive', new NotificationsArchive());
